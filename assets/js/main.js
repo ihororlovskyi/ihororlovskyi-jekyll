211a82a032b464515c9dfe0d875d81e896c39502
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-08-03 09:02:13 +0300

    experiments with refills

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,65 @@ $('.waypoint').each( function(i) {
 		}
 	}, { offset: '-25%' } );
 });
+
+
+/**
+* refills
+* http://refills.bourbon.io/
+*/
+
+
+/**
+* centered-navigation
+*/
+$(document).ready(function() {
+	var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
+	$('#js-centered-navigation-menu').removeClass("show");
+	
+	menuToggle.on('click', function(e) {
+		e.preventDefault();
+		$('#js-centered-navigation-menu').slideToggle(function(){
+			if($('#js-centered-navigation-menu').is(':hidden')) {
+				$('#js-centered-navigation-menu').removeAttr('style');
+			}
+		});
+	});
+});
+
+$(document).ready(function() {
+	if ($("#js-parallax-window").length) {
+		parallax();
+	}
+});
+
+$(window).scroll(function(e) {
+	if ($("#js-parallax-window").length) {
+		parallax();
+	}
+});
+
+
+/**
+* parallax-window
+*/
+function parallax(){
+	if( $("#js-parallax-window").length > 0 ) {
+		var plxBackground = $("#js-parallax-background");
+		var plxWindow = $("#js-parallax-window");
+
+		var plxWindowTopToPageTop = $(plxWindow).offset().top;
+		var windowTopToPageTop = $(window).scrollTop();
+		var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+		var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+		var windowInnerHeight = window.innerHeight;
+		var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+		var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+		var plxSpeed = 0.35;
+
+		plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+	}
+}
+
+
+
