0a6cdd7d1f154ba8e9bff18a67d2180f04032813
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-09-08 17:17:42 +0300

    add fancybox ajax popup

---
+++ _components/PopupMaker/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".various").fancybox({
+        maxWidth    : 660,
+        // maxHeight   : 600,
+        fitToView   : false,
+        width       : '70%',
+        height      : '70%',
+        autoSize    : true,
+        autoHeight    : true,
+        closeClick  : false,
+        openEffect  : 'none',
+        closeEffect : 'none'
+    });
+});
