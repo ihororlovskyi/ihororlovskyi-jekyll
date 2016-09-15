37e9d8e650f44fe01c290f2b6cb50144760f4ec4
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-09-16 02:23:52 +0300

    global cleaning :balloon:

--- _components/PopupMaker/js-jquery-modal.js
+++
@@ -1,5 +0,0 @@
-// $(document).ready(function() {
-//     $('.js-jquery-modal').modal({
-//         fadeDuration: 200
-//     });
-// });
--- assets/js/all.js
+++ assets/js/all.js
@@ -2054,24 +2054,3 @@ $('.js-back-fx').each( function(i) {
         }
     }, { offset: '-25%' } );
 });
-
-$(document).ready(function() {
-    $('.js-fancybox').fancybox({
-        maxWidth: 660,
-        // maxHeight: 600,
-        fitToView: false,
-        width: '70%',
-        height: '70%',
-        autoSize: true,
-        autoHeight: true,
-        closeClick: false,
-        openEffect: 'none',
-        closeEffect: 'none'
-    });
-});
-
-// $(document).ready(function() {
-//     $('.js-jquery-modal').modal({
-//         fadeDuration: 200
-//     });
-// });
