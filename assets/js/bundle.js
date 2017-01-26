a4e738acf018240fd3e89f2fd24542556da21219
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-01-27 00:10:46 +0200

    upd js-concat at gulpfile

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -10881,3 +10881,18 @@ https://github.com/imakewebthings/waypoints/blob/master/licenses.txt
   }
 }())
 ;
+// waypoints
+var $head = $( '.back-fx' );
+$('.js-back-fx').each( function(i) {
+    var $el = $( this ),
+        animClassDown = $el.data( 'animateDown' ),
+        animClassUp = $el.data( 'animateUp' );
+    $el.waypoint( function( direction ) {
+        if( direction === 'down' && animClassDown ) {
+            $head.attr('class', 'back-fx js-back-fx ' + animClassDown);
+        }
+        else if( direction === 'up' && animClassUp ){
+            $head.attr('class', 'back-fx js-back-fx ' + animClassUp);
+        }
+    }, { offset: '-25%' } );
+});
--- gulpfile.js/tasks/js-concat.js
+++ gulpfile.js/tasks/js-concat.js
@@ -5,7 +5,7 @@ gulp.task('js-concat', ['sass'], function () {
     return gulp.src([
             '_js/lib/*.js',
             '_js/vendor/*.js',
-            '_js/main.js'
+            '_components/**/*.js'
         ])
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('assets/js/'));
